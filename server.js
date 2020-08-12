6d3a2d4eca778625725c693e45d12d31ef254481
Nick Benoit <nick.benoit@utc.com>
2020-08-11 23:54:13 -0400

    nicks first commit

--- server.js
+++ server.js
@@ -1,40 +1,41 @@
 const express = require("express");
 const app = express();
-var bodyParser = require('body-parser');
+var bodyParser = require("body-parser");
 
-const client = require('twilio')(
-    process.env.TWILIO_ACCOUNT_SID,
-     process.env.TWILIO_AUTH_TOKEN
-
+const client = require("twilio")(
+  process.env.TWILIO_ACCOUNT_SID,
+  process.env.TWILIO_AUTH_TOKEN
 );
 const port = process.env.PORT || 8080;
 
 const http = require("http");
 const server = http.createServer(app);
 
-app.use(express.static('public'));
-app.get('/index.html', function (req, res) {
-    res.sendFile( __dirname + "/" + "index.html" );
-})
+app.use(bodyParser.json());
 
-app.use(express.urlencoded({ extended: false }));
-app.post('/process_form', function (req, res) {
-    // Prepare output in JSON format
-    console.log('Got body:', req.body);
-    phone_number = req.body.phone_number
-    //res.end(JSON.stringify(response));
-    client.messages.create({
-    from: process.env.TWILIO_PHONE_NUMBER,
-    to: phone_number,
-    body: "You just sent an SMS from Node.js using Twilio!"
-}).then((messsage) => console.log(message.sid));
-    res.sendStatus(200);
-})
+app.use(express.static("public"));
+app.get("/index.html", function (req, res) {
+  res.sendFile(__dirname + "/" + "index.html");
+});
+
+// app.use(express.urlencoded({ extended: false }));
+
+app.post("/process_form", function (req, res) {
+  // Prepare output in JSON format
+  phone_number = req.body.phone_number;
+  client.messages
+    .create({
+      from: process.env.TWILIO_PHONE_NUMBER,
+      to: phone_number,
+      body: "You just sent an SMS from Node.js using Twilio!",
+    })
+    .then((message) => console.log(message.sid));
+  res.sendStatus(200);
+});
 
 var serve_app = app.listen(port, function () {
-    var host = process.env.HOST  || "http://127.0.0.1";
-    var port = serve_app.address().port
+  var host = process.env.HOST || "http://127.0.0.1";
+  var port = serve_app.address().port;
 
-    console.log("Example app listening at %s:%s", host, port)
-})
-
+  console.log("Example app listening at %s:%s", host, port);
+});
